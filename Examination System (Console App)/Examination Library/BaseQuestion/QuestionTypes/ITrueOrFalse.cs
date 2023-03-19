ee4445753552c7eafa8a5a97dbb3f1328f9540a4
Hussien Ele <58689416+Hussien-Elepiary@users.noreply.github.com>
2023-03-19 09:33:36 +0200

    first lay out

---
+++ Examination System (Console App)/Examination Library/BaseQuestion/QuestionTypes/ITrueOrFalse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_Library.BaseQuestion.QuestionTypes
+{
+    internal interface ITrueOrFalse: IMainQuestion
+    {
+
+    }
+}
---
+++ Examination System (Console App)/Examination Library/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace Examination_Library
+{
+    internal interface Question
+    {
+        public string HeaderOfTheQuestion { get; set; }
+        public string BodyOfTheQuestion { get; set; }
+        public int Mark { get; set; }
+
+    }
+}
